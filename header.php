f9aff2fc4ee74e4dbdf1580dfc9d7dca8c96ae94
Parukia <dragontrainerryan724@yahoo.com>
2013-07-31 14:54:42 -0700

    Create header.php

---
+++ header.php
@@ -0,0 +1,6 @@
+<div id="header">
+<center>
+<a href="http://parukia.net"><img src="http://parukia.net/banner.png" alt="banner" /></a>
+</center>
+<link rel="shortcut icon" href="/favicon.ico" />
+</div>
