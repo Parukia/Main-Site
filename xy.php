8b654318506ca3b7e8a134da83cfbb2edba97ad2
Parukia <dragontrainerryan724@yahoo.com>
2013-07-31 14:58:00 -0700

    Create xy.php

---
+++ xy.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8" />
+<?php include 'slogan.php' ?>
+<link rel="stylesheet" type="text/css" 
+
+href="http://parukia.net/stylesheet.css">
+<title>Pokémon X and Y</title>
+<meta name="keywords" content="Pokémon, X, Y, Karos, Kalos, Generation Six, Pokemon, Latest, New, Newest" />
+</head>
+<body>
+
+<?php include 'header.php' ?>
+
+<?php include 'menu.php' ?>
+
+<div id="wrapper">
+
+
+  		<center>
+			<div id="content">
+				<h1><strong>Pokémon X & Y</strong></h1>
+				<p><img src="x-boxart.jpg" alt="Pokémon X Boxart"><img src="y-boxart.jpg" align="right" alt="Pokémon Y Boxart"></p>
+<br>
+<center><p>Pokémon X and Y are the first Generation 6 Pokémon games, due for worldwide release in October 2013.</p>
+
+<p>Taking place in the Kalos Region, these are the first Pokémon games to utilize the full effects of the Nintendo 3DS, including 3D battle scenes and an entire 3D world.</p>
+			</div>
+			</center>
+
+
+
+
+
+
+        </div>
+<?php include 'footer.php' ?>
+
+
+</body>
+</html>
