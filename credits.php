acf602ddcd131d38dbce6e76bca5ff0722247c69
Parukia <dragontrainerryan724@yahoo.com>
2013-07-31 14:52:48 -0700

    Create credits.php

---
+++ credits.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8" />
+
+<link rel="stylesheet" type="text/css" 
+
+href="http://parukia.net/stylesheet.css">
+<title>Credits - Parukia.Net</title>
+</head>
+<body>
+
+<?php include 'header.php' ?>
+
+<?php include 'menu.php' ?>
+
+<div id="csf">
+<div id="wrapper">
+
+
+  		<center>
+			<div id="content">
+				<h1>Credits</h1>
+				<br>
+				<p>Here, we give thanks to all the Parukians who contributed to the main website in one way or another. None of this would have been possible without the help of Parukia's community, as everything on Parukia is created by all of us from scratch. We greatly appreciate all contrubtions, big or small, that are made to help Parukia. However, every extremely large contribution is recorded here for everyone to see, as well as everyone who helped to build Parukia.Net. The following is a list of all contributors and their contributions for Parukia.Net.</p>
+				<br>
+				<h2>Parukia.Net Contributions</h2>
+				<br>
+				<p>Thanks to Frage for the banner and social media buttons of the site - you're one pro graphic designer!</p>
+				<p>Thanks to Uzumaki for finding this awesome background for us - it's so awesome!</p>
+				<p>And finally, thanks to Windex Drinker, Lugia, and Gary Oak for coding this entire website and putting everything together for Parukia!</p>
+			</div>
+			</center>
+
+
+
+
+
+
+        </div>
+<?php include 'footer.php' ?>
+
+
+</body>
+</html>
+</div>
+
+
+
+
+<!---TO DO LIST:
+
+ADD LINKS IN FOOTER, AS WELL AS DROPDOWN MENU, AND SIDEBAR-->
