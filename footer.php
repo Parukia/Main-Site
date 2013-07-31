bd2ee110984b05b60efbbb0c7831a7d9e458221d
Parukia <dragontrainerryan724@yahoo.com>
2013-07-31 14:54:04 -0700

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,54 @@
+<div class="push"></div>
+
+  <center>
+<div class="footer2">
+<table border="0" id="ft">
+  <tr>
+	  <td id="ftex" valign="top">
+		<h1>About us:</h1>
+            <p id="ftext"> <i>Copyright � 2012 � 2013 Parukia.Net. Pok&eacute;mon and 
+
+Pok&eacute;mon Chararacter names belong to Nintendo, Creatures, GAME FREAK, and The 
+
+Pok&eacute;mon Company International. Parukia.Net is not affiliated with Nintendo. All 
+
+content on this site belongs to their respective authors. Click <a 
+
+href="http://parukia.net/credits.php">here</a> to view our Credits page! Thanks 
+
+to Chris Spooner of line25.com for the dropdown menu! </i></p>
+	  </td>
+	  <td>
+<table border="0" id="sn">
+<h1>Stay Social:</h1>
+<tr><td><a href="http://parukianet.deviantart.com/">deviantArt</a></td></tr>
+<tr><td><a href="http://twitter.com/ParukiaNet">Twitter</a></td></tr>
+<tr><td><a href="http://facebook.com/ParukiaNet">Facebook</a></td></tr>
+</tr>
+</table>
+</td>
+	</tr>
+	</table>
+</div>
+        <div class="footer">
+        <table border="0" id="ft">
+	<tr>
+	  <td id="ftex">
+            <p id="ftext"> <i>Copyright � 2012 � 2013 Parukia.Net. Pokemon and 
+
+Pokemon Chararacter names belong to Nintendo, Creatures, GAME FREAK, and The 
+
+Pokemon Company International. Parukia.Net is not affiliated with Nintendo. All 
+
+content on this site belongs to their respective authors. Click <a 
+
+href="http://parukia.net/credits.php">here</a> to view our Credits page! Thanks 
+
+to Chris Spooner of line25.com for the dropdown menu! </i></p>
+	  </td>
+
+
+	</tr>
+	</table>
+        </div>
+	</center>
