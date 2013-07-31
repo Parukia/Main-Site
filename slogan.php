acef4b0da4ce97f47afe8b20a7c173b65c56855f
Parukia <dragontrainerryan724@yahoo.com>
2013-07-31 14:57:11 -0700

    Create slogan.php

---
+++ slogan.php
@@ -0,0 +1,2 @@
+<title>Parukia.Net - Online Pokemon Community</title>
+<meta name="keywords" content="Pokémon, X, Y, Kalos, Generation Six, Pokemon, Latest, New, Newest, GameFreak, Nintendo, Video Games" />
