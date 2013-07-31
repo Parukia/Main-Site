d9db8a3d8b02a380ca50c4f01e18482c3a80aaa7
Parukia <dragontrainerryan724@yahoo.com>
2013-07-31 14:56:05 -0700

    Create menu.php

---
+++ menu.php
@@ -0,0 +1,55 @@
+<div id="menu">
+
+<nav>
+  <ul>
+  	<li><a href="http://parukia.net">Home</a></li>
+		<li><a href="http://forums.parukia.net">Forums</a></li>
+		<li><form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+<input type="hidden" name="cmd" value="_donations">
+<input type="hidden" name="business" value="richardsonryan89@gmail.com">
+<input type="hidden" name="lc" value="US">
+<input type="hidden" name="item_name" value="Parukia">
+<input type="hidden" name="no_note" value="0">
+<input type="hidden" name="currency_code" value="USD">
+<input type="hidden" name="bn" value="PP-DonationsBF:btn_donate_LG.gif:NonHostedGuest">
+<input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donate_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+<img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
+</form></li>
+		<li><a href="http://parukia.net/about-us.php">About Us</a></li>
+		<li><a href="http://parukia.net/game-guides">In-Game Guides</a>
+			<ul>
+				<li><a href="http://parukia.net/game-guides/rby.php">Red/Blue/Yellow</a></li>
+				<li><a href="http://parukia.net/game-guides/gsc.php">Gold/Silver/Crystal</a></li>
+				<li><a href="http://parukia.net/game-guides/gen3.php">Generation III</a>
+					<ul>
+						<li><a href="http://parukia.net/game-guides/rse.php">Ruby/Sapphire/Emerald</a></li>
+						<li><a href="http://parukia.net/game-guides/frlg.php">FireRed/LeafGreen</a></li>
+					</ul>
+				</li>
+				<li><a href="http://parukia.net/game-guides/gen4.php">Generation IV</a>
+					<ul>
+						<li><a href="http://parukia.net/game-guides/dpp.php">Diamond/Pearl/Platinum</a></li>
+						<li><a href="http://parukia.net/game-guides/hgss.php">HeartGold/SoulSilver</a></li>
+					</ul>
+				</li>
+				<li><a href="http://parukia.net/game-guides/gen5.php">Generation V</a>
+					<ul>
+						<li><a href="http://parukia.net/game-guides/bw.php">Black/White</a></li>
+						<li><a href="#">Black2/White2</a></li>
+					</ul>
+				</li>
+				<li><a href="#">X/Y</a></li>
+			</ul>
+		</li>
+		<li><a href="http://parukia.net/competitive-guides">Competitive Guides</a>
+			<ul>
+				<li><a href="http://parukia.net/competitive-guides/strat-guides.php">Pokemon Strategy Guides</a></li>
+				<li><a href="http://parukia.net/competitive-guides/tiers.php">Tiers</a></li>
+				<li><a href="http://parukia.psim.us">Battle Simulator</a></li>
+			</ul>
+		</li>
+	</ul>
+</nav>
+
+
+</div>
