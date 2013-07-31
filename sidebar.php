6ff54accb906423aa8ccb29ff47e9e520ae3becc
Parukia <dragontrainerryan724@yahoo.com>
2013-07-31 14:56:46 -0700

    Create sidebar.php

---
+++ sidebar.php
@@ -0,0 +1,57 @@
+<div id="sidebar">
+<center>
+
+<table border="0" id="eventable">
+<tr>
+<td id="eventablepic"> <div id="nontran"><img 
+
+src="http://cdn.bulbagarden.net/upload/e/e7/386Deoxys.png" alt="Deoxys" 
+
+width="100%"></div>    </td>
+<td id="eventabletex"> <p id="evtext"><b>Deoxys</b></p>    </td>
+</tr>
+</table>
+<table border="0" id="countable">
+<tr> 
+<td id="countablepic"><img 
+
+src="http://upload.wikimedia.org/wikipedia/en/b/ba/Flag_of_Germany.svg" 
+
+title="Germany" height="" width="100%;" /></td>
+<td id="countablepic"><img 
+
+src="http://upload.wikimedia.org/wikipedia/en/c/c3/Flag_of_France.svg" 
+
+title="France" height="" width="100%;" /></td>
+</tr>
+
+<tr> 
+<td id="countablepic"><img 
+
+src="https://upload.wikimedia.org/wikipedia/en/9/9a/Flag_of_Spain.svg" 
+
+title="Spain" height="" width="100%;" /></td>
+<td id="countablepic"><img 
+
+src="http://upload.wikimedia.org/wikipedia/en/0/03/Flag_of_Italy.svg" 
+
+title="Italy" height="" width="100%;" /></td>
+</tr>
+
+
+</table>
+
+<br />
+<b>Start/End Date:</b> July 1 - July 22
+<br />
+<b>Where can I get it?:</b> Wi-Fi
+<br />
+<b>What games is it for?:</b> B2W2
+<br />
+<a href="http://parukia.net/events/2013/Mar/Pokemon_name">Other countries this 
+
+event is taking place in!</a>
+<br />
+</center>
+  	</div>
+</center>
